3b9e892b35be6d7aafecb4f68bed84ed2f084825
jimcheung <hi.jinhu.zhang@gmail.com>
2019-06-12 16:09:46 +0800

    fix verify_authenticity_token

--- lib/weixin_rails_middleware/helpers/responder.rb
+++ lib/weixin_rails_middleware/helpers/responder.rb
@@ -6,6 +6,12 @@ module WeixinRailsMiddleware
     include ReplyWeixinMessageHelper
 
     included do
+      if respond_to?(:verify_authenticity_token)
+        skip_before_action :verify_authenticity_token
+      else
+        skip_before_action :verify_authenticity_token, raise: false
+      end
+
       before_action :check_is_encrypt, only: [:index, :reply]
       before_action :initialize_adapter, :check_weixin_legality, only: [:index, :reply]
       before_action :set_weixin_public_account, :set_weixin_message, only: :reply
